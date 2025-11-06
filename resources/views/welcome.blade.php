63430e051068f786e926884e892fecf120c1ccad
rnavarr00 <rnavarro.arenas@gmail.com>
2025-11-06 16:02:28 +0100

    welcomeMaster.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,8 @@
-<!DOCTYPE html>
-<html lang="ca">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Benvingut a ListaCompra</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-</head>
+@extends('layouts.welcomeMaster')
+
+@section('title', 'Benvingut a ListaCompra')
+
+@section('content')
 <body class="bg-primary bg-gradient">
 
     <!-- Botons superior dret -->
@@ -21,7 +18,7 @@
     <!-- Contenidor central -->
     <div class="d-flex justify-content-center align-items-center vh-100">
         <div class="card shadow-lg p-4 p-md-5 text-center" style="max-width: 650px;">
-            <h1 class="text-primary fw-bold mb-3">Benvingut/da a ListaCompra 📝</h1>
+            <h1 class="text-primary fw-bold mb-3">Benvingut/da a llista compra 📝</h1>
             <p class="mb-3">
                 Aquesta aplicació t’ajuda a <strong>crear, organitzar i compartir llistes</strong> de forma fàcil i ràpida.  
                 Pots fer llistes de la compra, tasques o idees, i compartir-les amb els teus amics o companys per col·laborar-hi en temps real.
@@ -31,14 +28,15 @@
             </p>
 
             @guest
-                <a href="{{ route('register') }}" class="btn btn-primary btn-lg me-2">Crea un compte</a>
-                <a href="{{ route('login') }}" class="btn btn-outline-light btn-lg">Ja tinc compte</a>
+                <a href="{{ route('register') }}" class="btn btn-primary btn-lg mb-2">
+                <i class="bi bi-person-plus me-2"></i>Crea un compte</a>
+                <a href="{{ route('login') }}" class="btn btn-primary btn-lg">
+                <i class="bi bi-box-arrow-in-right me-2"></i>Ja tinc compte</a>
             @else
-                <a href="{{ url('/llistes') }}" class="btn btn-primary btn-lg">Accedeix a les teves llistes</a>
+                <a href="{{ url('/llistes') }}" class="btn btn-primary btn-lg">
+                Accedeix a les teves llistes</a>
             @endguest
         </div>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+@endsection
